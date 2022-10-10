ec20b93b3bb53e7c7246b33fc8eedec7667ea5fb
H.omura <64829133+omurais@users.noreply.github.com>
2022-10-10 18:55:58 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -52,15 +52,38 @@
     });
   }
 
+  function onScrollCallback(entries) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        toTop.classList.add('scrolled');
+      } else {
+        toTop.classList.remove('scrolled');
+      }
+    });
+  }
+
+  const toTop = document.getElementById('to_top');
+
   const options = {
     threshold: 0.2,
   };
 
   const observer = new IntersectionObserver(callback, options);
-
   const targets = document.querySelectorAll('.animate');
 
   targets.forEach(target => {
     observer.observe(target);
+  });
+
+  const onScrollObserver = new IntersectionObserver(onScrollCallback);
+  onScrollObserver.observe(document.getElementById('target'));
+
+  toTop.addEventListener('click', e => {
+    e.preventDefault();
+
+    window.scrollTo({
+      top:0,
+      behavior:'smooth'
+    });
   });
 }
