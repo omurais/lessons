8153ac30c390ac53f6e07727f0e5efbf355ceed5
H.omura <64829133+omurais@users.noreply.github.com>
2022-10-18 19:42:54 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 'use strict';
 
 {
+  //ハンバーガーメニュー開閉
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const close = document.getElementById('close');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
   // heroエリアMVスライドショー
   function play() {
     setTimeout(() => {
