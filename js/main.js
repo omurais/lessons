839f36953b768c6779d0fc035ba969b7f8e99999
omurais <i.t.a.n.o.m.t.h.u.b@gmail.com>
2022-10-23 17:22:35 +0900

    タブメニュー追加

--- js/main.js
+++ js/main.js
@@ -92,6 +92,26 @@
     });
   });
 
+  //タブメニュー
+  const menuItems = document.querySelectorAll('.menu li a');
+  const contents = document.querySelectorAll('.content');
+
+  menuItems.forEach(clickedItem => {
+    clickedItem.addEventListener('click', e => {
+      e.preventDefault();
+
+      menuItems.forEach(item => {
+        item.classList.remove('active');
+      });
+    clickedItem.classList.add('active');
+
+    contents.forEach(content => {
+      content.classList.remove('active');
+    });
+    document.getElementById(clickedItem.dataset.id).classList.add('active');
+    });
+  });
+
   //Topへ戻る
   const toTop = document.getElementById('to_top');
 
