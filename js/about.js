07a6881d059b6e3c7813b1048ca5d0902d538299
omurais <i.t.a.n.o.m.t.h.u.b@gmail.com>
2022-11-06 18:16:40 +0900

    モーダルウィンドウの修正

--- js/about.js
+++ js/about.js
@@ -6,10 +6,17 @@
    const abModal = document.getElementById('ab_modal');
    const abMask = document.getElementById('ab_mask');
  
+  //  aboutページ開くと表示
    window.addEventListener('load', () => {
      abModal.classList.remove('hidden');
      abMask.classList.remove('hidden');
    });
+
+  //  詳細を見るクリックで表示
+   abOpen.addEventListener('click', () => {
+    abModal.classList.remove('hidden');
+    abMask.classList.remove('hidden');
+  });
  
    abClose.addEventListener('click', () => {
      abModal.classList.add('hidden');
